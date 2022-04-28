1887fc86966864fcaad195420623a2a17c0ebf6c
DeVontae316 <DeVontaeMoore316@yahoo.com>
2022-04-28 06:08:53 -0500

    Added Redux boilerplate/README file

--- App.jsx
+++ App.jsx
@@ -1,16 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { StackNavigator } from "./src/navigation/StackNavigator";
+import { Provider } from "react-redux";
+import { store } from "./src/store/store";
 
 export default function App() {
-  return <StackNavigator />;
+  return (
+    <Provider store={store}>
+      <StackNavigator />
+    </Provider>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,9 @@
-import { Provider } from "react-redux";
 import createSagaMiddleware from "@redux-saga/core";
+import { configureStore } from "@reduxjs/toolkit";
+
+const saga = createSagaMiddleware();
+
+export const store = configureStore({
+  reducer: {},
+  middleware: [saga],
+});
