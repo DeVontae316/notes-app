30c0fafe7573bea0780d0b9301ee619e62475912
DeVontae316 <DeVontaeMoore316@yahoo.com>
2022-04-28 06:24:32 -0500

    Added api stateful values

---
+++ src/store/reducers/notesReducer/noteState.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const notesState = createSlice({
+  name: "notes",
+  initialState: {
+    notes: [],
+    isIdle: true,
+    isLoading: false,
+    isError: false,
+    isSuccess: false,
+  },
+  reducers: {
+    getNotes: (state) => {
+      state.isLoading = true;
+      state.isIdle = false;
+    },
+    getNotesSuccess: (state, action) => {
+      state.notes = action.payload;
+      state.isLoading = false;
+      state.isSuccess = true;
+    },
+    getNotesError: (state) => {
+      state.isLoading = false;
+      state.isError = true;
+    },
+  },
+});
